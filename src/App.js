f3539b6993c1a156a0121dde767b29b83dc18f6a
SSB <abdullahferdus@gmail.com>
2021-02-23 14:34:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Card from './components/card'
 import Card2 from './components/class'
 import Data from './data.json'
 import Name from './name.json'
+import State from './state'
 function App() {
     
     
@@ -10,6 +11,7 @@ function App() {
         <h1 className='headingStyle'>Hi</h1>
     {Data.map((item, index) => <Card key = {index} titleText={item.titleText} descText = {item.titleDesc}/>)}
         {Name.map((s_name, f_name) => <Card2 key = {f_name} name = {s_name.name} />)}
+        <State />
     </div>
 }
 
